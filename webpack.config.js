1106b6aa49fb1df362a5806f888a838e87c0120a
Pedro Ferreira <pedro.ferreira@wilmaa.com>
2023-06-18 14:40:31 +0200

    FIX: Clean the output folder before starting a new build.

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, `release/pair ${package.version}`),
+        clean: true,
     },
     module: {
         rules: [
