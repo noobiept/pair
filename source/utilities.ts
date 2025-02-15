a1672fe0593e6e53195643424463690ff6394244
Pedro Ferreira <plferreira4@gmail.com>
2025-02-15 01:24:03 +0100

    🐛: Don't allow wrapping of the grid when resizing.

--- source/utilities.ts
+++ source/utilities.ts
@@ -8,14 +8,3 @@ export function removeRandomElement<T>(array: T[]): T {
 
     return array.splice(position, 1)[0];
 }
-
-/**
- * Set a minimum width of the container so it doesn't ruin the grid as the view is resized.
- */
-export function adjustContainerWidth(
-    container: HTMLElement,
-    columnCount: number,
-    tileWidth: number,
-) {
-    container.style.minWidth = columnCount * tileWidth + 'px';
-}
